27d994fdd0bb63005a7c540e43310d577d7a26e9
Sérgio Ferreira <sergioferreira.ads@gmail.com>
2017-09-12 02:01:48 -0300

    Atualização conforme item 8 da APS01

---
+++ src/Restaurante/Prato.java
@@ -0,0 +1,34 @@
+package Restaurante;
+
+public class Prato {
+	
+	private String nome;
+	private double preco;
+	private String descricao;
+
+	public Prato(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
